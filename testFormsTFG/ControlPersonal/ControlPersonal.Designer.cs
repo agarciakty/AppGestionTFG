8241e056214d4d18147e27f61d86d70a5abf8bef
Ángel García <angelgarcia1311@hotmail.es>
2023-08-27 23:07:50 +0200

    Commit Final

--- testFormsTFG/ControlPersonal/ControlPersonal.Designer.cs
+++ testFormsTFG/ControlPersonal/ControlPersonal.Designer.cs
@@ -77,7 +77,7 @@
             this.dgvPersonal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvPersonal.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.dgvPersonal.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dgvPersonal.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dgvPersonal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvPersonal.Location = new System.Drawing.Point(190, 79);
